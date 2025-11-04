96232e829584625b54733df9a81d8815080afd0d
shiva raju <shivaraju529@gmail.com>
2025-11-04 08:05:00 +0530

    terraform

---
+++ Datasources/data.tf
@@ -0,0 +1,39 @@
+data "aws_ami" "joindevops" {
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+#     filter {
+#         name = "Architecture"
+#         values = ["x86_64"]
+#     }
+# }
+
+output "ami_id" {
+    value = data.aws_ami.joindevops
+}
+
+data "aws_instance" "mongodb"{
+
+    instance_id = "i-05924068aabbf9f7a"
+}
+
+output "mongob" {
+    value= data.aws_instance.mongodb.public_ip
+  
+}
